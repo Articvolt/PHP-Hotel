cfb88875bdeb8a6540d8584eb3551829acd25e03
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-10-06 14:59:10 +0200

    class chambe

---
+++ Chambre.php
@@ -0,0 +1,12 @@
+<?php
+
+    class Chambre {
+        private int $_num;
+        private int $_price;
+        private boolean $_wifi;
+
+        public function __construct(int $num, int $price, string $wifi) {
+            
+        }
+    }
+        
