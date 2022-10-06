dec0aa493149544977456fcd9d81d5f91d99c3fd
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-10-06 13:30:32 +0200

    Update Client.php

--- Client.php
+++ Client.php
@@ -8,6 +8,34 @@ class Client {
     public function __construct(string $username, string $name, int $nbrPersonn) {
         $this->_username = $username;
         $this->_name = $name;
-        $this->
+        $this->_nbrPersonn = $nbrPersonn;
+    }
+
+    //GETTER
+
+    public function getUsername() {
+        return $this->_username;
+    }
+
+    public function getName() {
+        return $this->_name;
+    }
+
+    public function getNbrPersonn() {
+        return $this->_nbrPersonn;
+    }
+
+    //SETTER
+
+    public function setUsername(string $NewUsername) {
+        $this->_username = $NewUsername;
+    }
+
+    public function setName(string $NewName) {
+        $this->_name = $NewName;
+    }
+
+    public function setNbrPersonne(int $NewNbrPersonn) {
+        $this->_nbrPersonn = $NewNbrPersonn;
     }
 }
