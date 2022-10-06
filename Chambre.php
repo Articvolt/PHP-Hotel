f3d81dc9680a6356342e65248c242ffe363033f3
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-10-06 15:03:31 +0200

    Update Chambre.php

--- Chambre.php
+++ Chambre.php
@@ -2,11 +2,12 @@
 
     class Chambre {
         private int $_num;
+        private int $_nbrBed;
         private int $_price;
         private boolean $_wifi;
 
-        public function __construct(int $num, int $price, string $wifi) {
-            
+        public function __construct(int $num, int $nbrBed, int $price, string $wifi) {
+
         }
     }
         
