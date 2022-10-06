3020abd42326a8ca1d50a6fcb601771a82ac4ca3
Ugo DA SILVA <ugodasilva67@gmail.com>
2022-10-06 11:40:08 +0200

    add files

---
+++ Client.php
@@ -0,0 +1,5 @@
+<?php
+
+class Client {
+    private string $_nom;
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
+</head>
+<body>
+    <h1>POO Hotel</h1>
+    <p>A partir de ces captures d'écran, réaliser l'application en POO permettant la gestion de réservations de chambres par des clients dans différents hôtels:</p>
+    <img src="img/hotel1.png" alt="">
+    <img src="img/hotel2.png" alt="">
+    <img src="img/hotel3.png" alt="">
+    <img src="img/hotel4.png" alt="">
+</body>
+</html>
